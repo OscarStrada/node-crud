94ff8297c25ed1dc8f99e617cb7c4b35de0287d6
Oscar Estrada <arturostrada2020@gmail.com>
2022-05-26 19:55:50 -0500

    handlebars added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
+import { create } from "express-handlebars";
 import routes from "./routes/index.routes";
+import path from "path";
+
+const app = express();
 
 // settings
-const app = express();
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
 
+// handlebars
+const exphbs = create({
+  extname: ".hbs",
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  defaultLayout: "main",
+});
+
+app.engine(".hbs", exphbs.engine);
+app.set("view engine", ".hbs");
+
+// routes
 app.use(routes);
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
 export default router;
